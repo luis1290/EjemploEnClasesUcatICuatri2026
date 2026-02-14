658e7e0d131094d4fb48dd6eb4afd7dfe48e815f
Luis <lgomez1290@gmail.com>
2026-02-13 19:11:25 -0600

    agregar comentarion en archivo main.py

--- semana4/MVC/main.py
+++ semana4/MVC/main.py
@@ -1,8 +1,8 @@
 
 from controller.UserController import UserController # Importamos la clase UserController desde el módulo controller.UserController
 
+# Punto de entrada del programa
 if __name__ == "__main__":
     controller = UserController() # Creamos una instancia de UserController
-    controller.createUser("Juan Perez", "juan@gmail.com") # Llamamos al método createUser para crear un nuevo usuario con el nombre "Juan Perez" y el correo electrónico "  
-    
+    controller.createUser("Juan Perez", "juan@gmail.com") # Llamamos al método createUser para crear un nuevo usuario con el nombre "Juan Perez" y el correo electrónico   
 
