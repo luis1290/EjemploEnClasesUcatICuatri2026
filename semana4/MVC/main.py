c66bee5e87819a3dc9cea041e60ecdc99295d0ef
Luis <lgomez1290@gmail.com>
2026-03-09 09:36:14 -0600

    MVC Semana4

--- semana4/MVC/main.py
+++ semana4/MVC/main.py
@@ -4,5 +4,5 @@ from controller.UserController import UserController # Importamos la clase UserC
 # Punto de entrada del programa
 if __name__ == "__main__":
     controller = UserController() # Creamos una instancia de UserController
-    controller.createUser("Juan Perez", "juan@gmail.com") # Llamamos al método createUser para crear un nuevo usuario con el nombre "Juan Perez" y el correo electrónico   
+    controller.createUser("Luis", "luis@gmail.com") # Llamamos al método createUser para crear un nuevo usuario con el nombre "Juan Perez" y el correo electrónico   
 
